00923a8d2beca4952510eec2527a655fa77feb9f
Havi S <havi@blitterfly.com>
2016-09-21 09:35:04 -0500

    Move GUI files to subdir, there's gonna be more

--- src/gui/guipanel.cpp
+++ src/gui/guipanel.cpp
@@ -20,10 +20,10 @@ void GuiPanel::OnRender(SDL_Renderer * renderer, const CB_ViewClippingInfo & cli
 void GuiPanel::OnDebugRender(SDL_Renderer * renderer, const CB_ViewClippingInfo & clip_info) {
     if (clip_info.z_index == ZIndex::Gui) {
         rectangleRGBA(renderer, clip_info.dest.x, clip_info.dest.y, clip_info.dest.right(), clip_info.dest.bottom(),
-                        127, 0, 127, 127);
+                        255, 69, 0, 127);
         std::string label = this->panel_name + ":" + std::to_string(this->entity_id);
         boxRGBA(renderer, clip_info.dest.x, clip_info.dest.bottom(), clip_info.dest.x + label.length() * 8 + 2,
-                clip_info.dest.bottom() + 10, 127, 0, 127, 127);
+                clip_info.dest.bottom() + 10, 255, 69, 0, 127);
         stringRGBA(renderer, clip_info.dest.x + 1, clip_info.dest.bottom() + 1, label.c_str(), 255, 255, 255,
                     255);
     }
